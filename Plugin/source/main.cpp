957c86df5b5d576cb4173a7f35fde57763cd7ea4
arabian <a.tevg@ya.ru>
2023-08-17 17:35:08 +0300

    Changed the file paths in "include" to match the real registry. This will fix the build of the project for linux users

--- Plugin/source/main.cpp
+++ Plugin/source/main.cpp
@@ -1,8 +1,8 @@
 #include <switch_min.h>
 
-#include "saltysd/SaltySD_core.h"
-#include "saltysd/SaltySD_ipc.h"
-#include "saltysd/SaltySD_dynamic.h"
+#include "saltysd/saltysd_core.h"
+#include "saltysd/saltysd_ipc.h"
+#include "saltysd/saltysd_dynamic.h"
 
 extern "C" {
 	extern u32 __start__;
